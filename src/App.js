0e05af12db90cff38c0f605af0cfb1a79b49befb
Kamareon59 <93917055+Kamareon59@users.noreply.github.com>
2022-04-16 20:28:03 +0200

    Add basic HTML + CSS structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <p>Weather app</p>
+      <div className="container-fluid">
+        <Weather />
+        <footer>
+          This project was{" "}
+          <a
+            href="https://github.com/Kamareon59/react-weather-app"
+            rel="noreferrer"
+          >
+            coded
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://marit-versteeg-portfolio.netlify.app/"
+            rel="noreferrer"
+          >
+            Marit Versteeg
+          </a>{" "}
+          and hosted via{" "}
+          <a href="https://www.netlify.com" rel="noreferrer">
+            Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
